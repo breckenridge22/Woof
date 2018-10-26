92ea79ce1b15ad9ddad51cc24b2778c700cd5649
Drake <pocsatko.2@buckeyemail.osu.edu>
2018-10-25 21:00:49 -0400

    Added reauthentication to the delete page

--- app/src/main/java/com/osu/cse/apps/mobile/woof/DeleteFragment.java
+++ app/src/main/java/com/osu/cse/apps/mobile/woof/DeleteFragment.java
@@ -4,15 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -23,7 +28,14 @@ public class DeleteFragment extends Fragment implements View.OnClickListener {
 
     private Button mYesButton;
     private Button mNoButton;
+    private Button mReauthenticateButton;
+    private EditText mEmailText;
+    private EditText mPasswordText;
+    private LinearLayout mReauthLayout;
+    private LinearLayout mDeleteLayout;
     private static final String TAG = "DeleteFragment";
+    private FirebaseUser mUser;
+
 
 
 
@@ -42,6 +54,12 @@ public class DeleteFragment extends Fragment implements View.OnClickListener {
 
         mYesButton = v.findViewById(R.id.yes_button);
         mNoButton = v.findViewById(R.id.no_button);
+        mReauthenticateButton = v.findViewById(R.id.reauthenticate_delete);
+        mEmailText = v.findViewById(R.id.re_email);
+        mPasswordText = v.findViewById(R.id.re_password);
+        mReauthLayout = v.findViewById(R.id.reauth_layout);
+        mDeleteLayout = v.findViewById(R.id.delete_layout);
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
 
 
         if (mYesButton != null) {
@@ -50,8 +68,61 @@ public class DeleteFragment extends Fragment implements View.OnClickListener {
         if (mNoButton != null) {
             mNoButton.setOnClickListener(this);
         }
-
+        if (mReauthenticateButton != null) {
+            mReauthenticateButton.setOnClickListener(this);
+        }
         return v;
+    }
+
+    // Skeleton from https://firebase.google.com/docs/auth/android/manage-users
+    public void reauthenticate() {
+        if(validateSignInForm()) {
+            AuthCredential credential = EmailAuthProvider
+                    .getCredential(mEmailText.getText().toString(), mPasswordText.getText().toString());
+
+            // Prompt the user to re-provide their sign-in credentials
+            mUser.reauthenticate(credential)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "User re-authenticated.");
+                                mReauthLayout.setVisibility(View.GONE);
+                                mDeleteLayout.setVisibility(View.VISIBLE);
+                                Toast.makeText(getActivity(), "Successfully re-authenticated", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Log.d(TAG, "Re-authentication failed.");
+                                Toast.makeText(getActivity(), "Re-authentication failed.", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+        } else {
+            Toast.makeText(getActivity(), "Please fill in the form.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Skeleton from https://github.com/firebase/quickstart-android/blob/master/auth/app/src/main/java/com/google/firebase/quickstart/auth/java/EmailPasswordActivity.java
+    private boolean validateSignInForm() {
+        boolean valid = true;
+
+        String userName = mEmailText.getText().toString();
+        if (TextUtils.isEmpty(userName)) {
+            mEmailText.setError("Required.");
+            valid = false;
+        } else {
+            mEmailText.setError(null);
+        }
+
+        String password = mPasswordText.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordText.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordText.setError(null);
+        }
+
+        return valid;
     }
 
     public void onClick(View v) {
@@ -59,6 +130,10 @@ public class DeleteFragment extends Fragment implements View.OnClickListener {
         Intent intent;
         FirebaseUser aUser = mAuth.getCurrentUser();
         switch(i){
+            case R.id.reauthenticate_delete:
+                Log.d(TAG, "Reauth button pressed");
+                reauthenticate();
+                break;
             case R.id.yes_button:
                 aUser.delete()
                     .addOnCompleteListener(new OnCompleteListener<Void>() {
