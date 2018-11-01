77f84114656e9868ee77e676f73420e37765f5e2
Drake <pocsatko.2@buckeyemail.osu.edu>
2018-11-01 19:36:32 -0400

    Use fragment for map and enable location

--- app/src/main/java/com/osu/cse/apps/mobile/woof/MapsActivity.java
+++ app/src/main/java/com/osu/cse/apps/mobile/woof/MapsActivity.java
@@ -3,16 +3,35 @@ package com.osu.cse.apps.mobile.woof;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
+import com.mapbox.mapboxsdk.maps.SupportMapFragment;
 
-public class MapsActivity extends AppCompatActivity {
-    private MapView mapView;
+import java.util.List;
+
+
+// Most of this comes from https://github.com/mapbox/mapbox-android-demo/blob/master/MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/plugins/LocationComponentFragmentActivity.java
+// and its corresponding xml file
+public class MapsActivity extends AppCompatActivity implements PermissionsListener {
+
+    private MapboxMap mapboxMap;
+    private PermissionsManager permissionsManager;
 
     private static final String TAG = "MapsActivity";
 
@@ -24,63 +43,80 @@ public class MapsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "On create called");
+        setContentView(R.layout.activity_maps);
+
+        // Mapbox access token is configured here. This needs to be called either in your application
+        // object or in the same activity which contains the mapview.
         Mapbox.getInstance(this, getString(R.string.mapbox_key));
 
-        Log.d(TAG, "Map Instance retrieved");
-        setContentView(R.layout.activity_maps);
-        mapView = (MapView) findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
+        // Create supportMapFragment
+        SupportMapFragment mapFragment;
+        if (savedInstanceState == null) {
+
+            // Create fragment
+            final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            LatLng office = new LatLng(38.899895, -77.03401);
+
+            // Build mapboxMap
+            MapboxMapOptions options = new MapboxMapOptions();
+            options.camera(new CameraPosition.Builder()
+                    .target(office)
+                    .zoom(9)
+                    .build());
+
+            // Create map fragment
+            mapFragment = SupportMapFragment.newInstance(options);
+
+            // Add map fragment to parent container
+            transaction.add(R.id.location_frag_container, mapFragment, "com.mapbox.map");
+            transaction.commit();
+        } else {
+            mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentByTag("com.mapbox.map");
+        }
+
+        mapFragment.getMapAsync(mapboxMap -> {
+            MapsActivity.this.mapboxMap = mapboxMap;
+            enableLocationComponent();
+        });
     }
 
-    public void onMapReady(MapboxMap mapboxMap) {
-
-        LocationComponent locationComponent = mapboxMap.getLocationComponent();
-
-        // TODO: figure out how to fix this, checkpoint 6? NFC: security
-        locationComponent.activateLocationComponent(this);
-        locationComponent.setLocationComponentEnabled(true);
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationComponent() {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            // Get an instance of the location component. Adding in LocationComponentOptions is also an optional
+            // parameter
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+            locationComponent.activateLocationComponent(this);
+            locationComponent.setLocationComponentEnabled(true);
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+            locationComponent.setRenderMode(RenderMode.NORMAL);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        mapView.onStart();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        mapView.onResume();
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        // TODO fix this tost
+        Toast.makeText(this, "Shut up", Toast.LENGTH_LONG).show();
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            enableLocationComponent();
+        } else {
+            // TODO fix the toast
+            Toast.makeText(this, "Shut up", Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 }
