af8b9212841f1dc12228c9c355d19f4150700437
Drake <pocsatko.2@buckeyemail.osu.edu>
2018-11-01 20:50:31 -0400

    Enable optimized route planning based on points

--- app/src/main/java/com/osu/cse/apps/mobile/woof/MapsActivity.java
+++ app/src/main/java/com/osu/cse/apps/mobile/woof/MapsActivity.java
@@ -2,6 +2,7 @@ package com.osu.cse.apps.mobile.woof;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentTransaction;
@@ -11,7 +12,16 @@ import android.widget.Toast;
 
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.api.directions.v5.DirectionsCriteria;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.api.optimization.v1.MapboxOptimization;
+import com.mapbox.api.optimization.v1.models.OptimizationResponse;
+import com.mapbox.geojson.LineString;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.annotations.Polyline;
+import com.mapbox.mapboxsdk.annotations.PolylineOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -23,15 +33,33 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
 import com.mapbox.mapboxsdk.maps.SupportMapFragment;
 
+import static com.mapbox.core.constants.Constants.PRECISION_6;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 // Most of this comes from https://github.com/mapbox/mapbox-android-demo/blob/master/MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/plugins/LocationComponentFragmentActivity.java
 // and its corresponding xml file
-public class MapsActivity extends AppCompatActivity implements PermissionsListener {
+// Also here https://www.mapbox.com/android-docs/java/examples/generate-an-optimized-route/
+public class MapsActivity extends AppCompatActivity implements PermissionsListener,
+        MapboxMap.OnMapClickListener, MapboxMap.OnMapLongClickListener {
 
     private MapboxMap mapboxMap;
     private PermissionsManager permissionsManager;
+    private DirectionsRoute optimizedRoute;
+    private MapboxOptimization optimizedClient;
+    private Polyline optimizedPolyline;
+    private List<Point> stops;
+    // constant strings for the optimized route
+    private static final String FIRST = "first";
+    private static final String ANY = "any";
+    private static final String TEAL_COLOR = "#23D2BE";
+    private static final int POLYLINE_WIDTH = 5;
 
     private static final String TAG = "MapsActivity";
 
@@ -44,6 +72,7 @@ public class MapsActivity extends AppCompatActivity implements PermissionsListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        stops = new ArrayList<>();
 
         // Mapbox access token is configured here. This needs to be called either in your application
         // object or in the same activity which contains the mapview.
@@ -56,12 +85,9 @@ public class MapsActivity extends AppCompatActivity implements PermissionsListen
             // Create fragment
             final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
-            LatLng office = new LatLng(38.899895, -77.03401);
-
             // Build mapboxMap
             MapboxMapOptions options = new MapboxMapOptions();
             options.camera(new CameraPosition.Builder()
-                    .target(office)
                     .zoom(9)
                     .build());
 
@@ -77,11 +103,122 @@ public class MapsActivity extends AppCompatActivity implements PermissionsListen
 
         mapFragment.getMapAsync(mapboxMap -> {
             MapsActivity.this.mapboxMap = mapboxMap;
+            mapboxMap.addOnMapClickListener(this);
+            mapboxMap.addOnMapLongClickListener(this);
             enableLocationComponent();
         });
     }
 
-    @SuppressWarnings( {"MissingPermission"})
+    @Override
+    public void onMapClick(@NonNull LatLng point) {
+        // Optimization API is limited to 12 coordinate sets
+        if (alreadyTwelveMarkersOnMap()) {
+            // TODO fix the toast
+            Toast.makeText(MapsActivity.this, "Cut it out.", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d(TAG, "onMapClick called");
+            addDestinationMarker(point);
+            addPointToStopsList(point);
+            if (stops.size() >= 2) {
+                getOptimizedRoute(stops);
+            }
+        }
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng point) {
+        Log.d(TAG, "onMapLongClick called");
+        mapboxMap.clear();
+        stops.clear();
+        //addFirstStopToStopsList();
+    }
+
+    private boolean alreadyTwelveMarkersOnMap() {
+        if (stops.size() == 12) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    private void addPointToStopsList(LatLng point) {
+        stops.add(Point.fromLngLat(point.getLongitude(), point.getLatitude()));
+    }
+
+    private void addDestinationMarker(LatLng point) {
+        Log.d(TAG, "addDestinationMarker called");
+        mapboxMap.addMarker(new MarkerOptions()
+                .position(new LatLng(point.getLatitude(), point.getLongitude()))
+                .title("Destination")); // TODO fix this string
+    }
+
+
+    private void getOptimizedRoute(List<Point> coordinates) {
+        optimizedClient = MapboxOptimization.builder()
+                .source(FIRST)
+                .destination(ANY)
+                .coordinates(coordinates)
+                .overview(DirectionsCriteria.OVERVIEW_FULL)
+                .profile(DirectionsCriteria.PROFILE_DRIVING)
+                .accessToken(Mapbox.getAccessToken())
+                .build();
+
+        optimizedClient.enqueueCall(new Callback<OptimizationResponse>() {
+            @Override
+            public void onResponse(Call<OptimizationResponse> call, Response<OptimizationResponse> response) {
+                if (!response.isSuccessful()) {
+                    Log.d("DirectionsActivity", "Nope"); // TODO fix with string
+                    Toast.makeText(MapsActivity.this, "Nope", Toast.LENGTH_SHORT).show(); // TODO fix with string
+                    return;
+                } else {
+                    if (response.body().trips().isEmpty()) {
+                        Log.d("DirectionsActivity", "Yes, but no" + " size = " + response.body().trips().size());
+                        Toast.makeText(MapsActivity.this, "Yes, but no", // TODO fix with string
+                                Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+
+                // Get most optimized route from API response
+                optimizedRoute = response.body().trips().get(0);
+                drawOptimizedRoute(optimizedRoute);
+            }
+
+            @Override
+            public void onFailure(Call<OptimizationResponse> call, Throwable throwable) {
+                Log.d("DirectionsActivity", "Error: " + throwable.getMessage());
+            }
+        });
+    }
+
+    private void drawOptimizedRoute(DirectionsRoute route) {
+            // Remove old polyline
+        if (optimizedPolyline != null) {
+            mapboxMap.removePolyline(optimizedPolyline);
+        }
+        // Draw points on MapView
+        LatLng[] pointsToDraw = convertLineStringToLatLng(route);
+        optimizedPolyline = mapboxMap.addPolyline(new PolylineOptions()
+                .add(pointsToDraw)
+                .color(Color.parseColor(TEAL_COLOR))
+                .width(POLYLINE_WIDTH));
+    }
+
+    private LatLng[] convertLineStringToLatLng(DirectionsRoute route) {
+        // Convert LineString coordinates into LatLng[]
+        LineString lineString = LineString.fromPolyline(route.geometry(), PRECISION_6);
+        List<Point> coordinates = lineString.coordinates();
+        LatLng[] points = new LatLng[coordinates.size()];
+        for (int i = 0; i < coordinates.size(); i++) {
+            points[i] = new LatLng(
+                    coordinates.get(i).latitude(),
+                    coordinates.get(i).longitude());
+        }
+        return points;
+    }
+
+    @SuppressWarnings({"MissingPermission"})
     private void enableLocationComponent() {
         // Check if permissions are enabled and if not request
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
@@ -105,7 +242,7 @@ public class MapsActivity extends AppCompatActivity implements PermissionsListen
 
     @Override
     public void onExplanationNeeded(List<String> permissionsToExplain) {
-        // TODO fix this tost
+        // TODO fix this toast
         Toast.makeText(this, "Shut up", Toast.LENGTH_LONG).show();
     }
 
